2e62aab8393984a249c9fb6d04f2217649bf88c9
Alessandro Scala <alessandro.scala8@gmail.com>
2024-04-17 10:41:33 +0200

    Github actions update

--- wrap/WindingNumber/SolidAngle.h
+++ wrap/WindingNumber/SolidAngle.h
@@ -1626,9 +1626,13 @@ public:
     { T *data = myData; myData = newdata; return data; }
 
     template <typename IT, bool FORWARD>
-    class base_iterator : 
-	public std::iterator<std::random_access_iterator_tag, T, exint> 
+    class base_iterator
+	//: public std::iterator<std::random_access_iterator_tag, T, exint>    DEPRECATED
     {
+        using iterator_category = std::random_access_iterator_tag;
+        using value_type = T;
+        using difference_type = exint;
+        
         public:
 	    typedef IT&		reference;
 	    typedef IT*		pointer;
